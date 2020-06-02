e638d3690c85637211cbf241f6645936d7d710e4
Saksham Sharda <saksham20.sharda@gmail.com>
2020-06-02 11:24:07 -0400

    extract only one plane

--- segmentationextractors/suite2p/suite2psegmentationextractor.py
+++ segmentationextractors/suite2p/suite2psegmentationextractor.py
@@ -1,12 +1,10 @@
 import numpy as np
 from segmentationextractors import SegmentationExtractor
-from suite2p import run_s2p
-from suite2p.io import nwb as s2p_nwb
+import os
 
 
 class Suite2pSegmentationExtractor(SegmentationExtractor):
-
-    def __init__(self, fileloc, op=None, db=None, s2pobj=None):
+    def __init__(self, fileloc):
         """
         Creating SegmentationExtractor object out of suite 2p data type.
         Parameters
@@ -16,37 +14,25 @@ class Suite2pSegmentationExtractor(SegmentationExtractor):
         db: dict
             db overwrites any ops (allows for experiment specific settings)
         """
-        if op is None:
-            op = run_s2p.default_ops()
-        if db is None:
-            db = dict(data_path=[fileloc])
-        else:
-            db.update(data_path=[fileloc])
-        if s2pobj is None:
-            self.s2p_obj = run_s2p.run_s2p(ops=op, db=db)
-        else:
-            self.s2p_obj = s2pobj
-
-        if len(op['data_path'] == 0):
-            self.savepath = fileloc + '\suite2p'
-        else:
-            self.savepath = op['data_path'][0] + '\suite2p'
-        self.fileloc = fileloc
-        self.op_inp = op
-        self.no_channels = op['nchannels']
-        self.no_planes = op['nplanes']
+        self.filepath = fileloc
+        self.no_planes_extract=1
         self.stat = self._load_npy('stat.npy')
         self.F = self._load_npy('F.npy',mmap_mode='r')
         self.Fneu = self._load_npy('Fneu.npy',mmap_mode='r')
         self.spks = self._load_npy('spks.npy',mmap_mode='r')
         self.iscell = self._load_npy('iscell.npy',mmap_mode='r')
         self.ops = [i.item() for i in self._load_npy('ops.npy')]
+        self.op_inp = self.ops[0]
+        self.no_channels = self.op_inp['nchannels']
+        self.no_planes = self.op_inp['nplanes']
         self.rois_per_plane = [i.shape[0] for i in self.iscell]
+        self.raw_images = None
+        self.roi_response = None
 
     def _load_npy(self, filename, mmap_mode=None):
-        ret_val = [[None]]*self.no_planes
-        for i in range(self.no_planes):
-            ret_val[i] = np.load(self.savepath + f'\\Plane{i}\\' + filename,
+        ret_val = [[None]]*self.no_planes_extract
+        for i in range(self.no_planes_extract):
+            ret_val[i] = np.load(self.filepath + f'\\Plane{i}\\' + filename,
                                  mmap_mode=mmap_mode,
                                  allow_pickle=not mmap_mode and True)
         return ret_val
@@ -65,13 +51,13 @@ class Suite2pSegmentationExtractor(SegmentationExtractor):
 
     @property
     def accepted_list(self):
-        plane_wise = [np.where(i[:, 0] == 1) for i in self.iscell]
-        return [plane_wise[0],len(self.stat[0])+plane_wise[0]]
+        plane_wise = [np.where(i[:, 0] == 1)[0] for i in self.iscell]
+        return [plane_wise[0].tolist(),(len(self.stat[0])+plane_wise[0]).tolist()][0:self.no_planes_extract]
 
     @property
     def rejected_list(self):
-        plane_wise = [np.where(i[:, 0] == 0) for i in self.iscell]
-        return [plane_wise[0], len(self.stat[0]) + plane_wise[0]]
+        plane_wise = [np.where(i[:, 0] == 0)[0] for i in self.iscell]
+        return [plane_wise[0].tolist(), (len(self.stat[0]) + plane_wise[0]).tolist()][0:self.no_planes_extract]
 
     @property
     def roi_locs(self):
@@ -87,11 +73,11 @@ class Suite2pSegmentationExtractor(SegmentationExtractor):
 
     @property
     def samp_freq(self):
-        return self.ops_inp['fs']*self.no_planes
+        return self.ops[0]['fs']*self.no_planes
 
     @staticmethod
     def write_recording(nwb_file_path):
-        return s2p_nwb.read(nwb_file_path)
+        return NotImplementedError
 
     # defining the abstract class enforced methods:
     def get_roi_ids(self):
@@ -126,12 +112,12 @@ class Suite2pSegmentationExtractor(SegmentationExtractor):
             ROI_idx = [np.where(np.array(i) == self.roi_idx)[0] for i in ROI_ids]
             ele = [i for i, j in enumerate(ROI_idx) if j.size == 0]
             ROI_idx_ = [j[0] for i, j in enumerate(ROI_idx) if i not in ele]
-        if name=='fluorescence':
-            return np.concatenate(self.F)[[ROI_idx_],start_frame:end_frame]
-        if name=='neuropil':
-            return np.concatenate(self.Fneu)[[ROI_idx_],start_frame:end_frame]
-        if name=='deconvolved':
-            return np.concatenate(self.spks)[[ROI_idx_],start_frame:end_frame]
+        if name=='Fluorescence':
+            return np.concatenate(self.F[0:self.no_planes_extract])[[ROI_idx_],start_frame:end_frame].squeeze()
+        if name=='Neuropil':
+            return np.concatenate(self.Fneu[0:self.no_planes_extract])[[ROI_idx_],start_frame:end_frame].squeeze()
+        if name=='Deconvolved':
+            return np.concatenate(self.spks[0:self.no_planes_extract])[[ROI_idx_],start_frame:end_frame].squeeze()
         else:
             return None
 
@@ -141,25 +127,26 @@ class Suite2pSegmentationExtractor(SegmentationExtractor):
     def get_pixel_masks(self, ROI_ids=None):
         pixel_mask = [None]*self.no_rois
         c = 0
-        for i in self.no_planes:
-            for j in self.rois_per_plane[i]:
-                c +=1
+        for i in range(self.no_planes_extract):
+            for j in range(self.rois_per_plane[i]):
                 pixel_mask[c] = np.array([self.stat[i][j]['ypix'],
-                                       self.stat[i][j]['xpix'],
-                                       self.stat[i][j]['lam']])
+                                         self.stat[i][j]['xpix'],
+                                         self.stat[i][j]['lam'],
+                                         c*np.ones(self.stat[i][j]['lam'].size)]).T
+                c += 1
         if ROI_ids is None:
             ROI_idx_ = range(self.get_num_rois())
         else:
             ROI_idx = [np.where(np.array(i) == self.roi_idx)[0] for i in ROI_ids]
             ele = [i for i, j in enumerate(ROI_idx) if j.size == 0]
             ROI_idx_ = [j[0] for i, j in enumerate(ROI_idx) if i not in ele]
-        return [pixel_mask[i] for i in ROI_idx_]
+        return np.concatenate([pixel_mask[i] for i in ROI_idx_])
 
     def get_movie_framesize(self):
         return self.image_dims
 
     def get_movie_location(self):
-        return self.fileloc
+        return os.path.abspath(os.path.join(self.filepath, os.path.pardir))
 
     def get_channel_names(self):
         return [f'OpticalChannel{i}' for i in range(self.no_channels)]
