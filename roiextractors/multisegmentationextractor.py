7de46565e291e704459dba59e0f97a636efdfdf9
Saksham Sharda <saksham20.sharda@gmail.com>
2020-08-24 22:55:47 -0500

    using decorators

--- roiextractors/multisegmentationextractor.py
+++ roiextractors/multisegmentationextractor.py
@@ -4,6 +4,22 @@ from .segmentationextractor import SegmentationExtractor
 from . import *
 from .extractorlist import *
 
+def concatenate_output(func):
+    def _get_from_roi_map(self, roi_ids=None, **kwargs):
+        out = []
+        if roi_ids is None:
+            roi_ids = np.array(self._all_roi_ids)
+        else:
+            roi_ids = np.array(roi_ids)
+        seg_id = np.array([self._roi_map[roi_id]['segmentation_id'] for roi_id in roi_ids])
+        roi_id_segmentation = np.array([self._roi_map[roi_id]['roi_id'] for roi_id in roi_ids])
+        for i in np.unique(seg_id):
+            seg_roi_ids = roi_id_segmentation[seg_id == i]
+            out.append(getattr(self._segmentations[i],func.__name__)(roi_ids=seg_roi_ids, **kwargs))
+        return func(self)(out)
+    return _get_from_roi_map
+
+
 class MultiSegmentationExtractor(SegmentationExtractor):
     """
     This class is used to concatenate multiplane recordings from the same device and session
@@ -75,74 +91,37 @@ class MultiSegmentationExtractor(SegmentationExtractor):
         return_dict = dict()
         for i in range(self._no_planes):
             for image_name, image in self._segmentations[i].get_images_dict().items():
-                return_dict.update({image_name+f'_Plane{i}': image})
+                return_dict.update({f'{image_name}_Plane{i}': image})
         return return_dict
 
     def get_traces_dict(self):
         return_dict = dict()
         for i in range(self._no_planes):
             for trace_name, trace in self._segmentations[i].get_traces_dict().items():
-                return_dict.update({trace_name + f'_Plane{i}': trace})
+                return_dict.update({f'{trace_name}_Plane{i}': trace})
         return return_dict
 
     def get_image_size(self):
         return self._segmentations[0].get_image_size()
 
+    @concatenate_output
     def get_traces(self, roi_ids=None, start_frame=None, end_frame=None, name='Fluorescence'):
-        traces_all = []
-        if roi_ids is None:
-            roi_ids = np.array(self._all_roi_ids)
-        else:
-            roi_ids = np.array(roi_ids)
-        seg_id = np.array([self._roi_map[roi_id]['segmentation_id'] for roi_id in roi_ids])
-        roi_id_segmentation = np.array([self._roi_map[roi_id]['roi_id'] for roi_id in roi_ids])
-        for i in np.unique(seg_id):
-            seg_roi_ids = roi_id_segmentation[seg_id==i]
-            traces_all.append(self._segmentations[i].get_traces(
-                roi_ids=seg_roi_ids, start_frame=start_frame, end_frame=end_frame, name=name))
-        return np.concatenate(traces_all, axis=0)
+        return lambda x: np.concatenate(x, axis=0)
 
+    @concatenate_output
     def get_roi_pixel_masks(self, roi_ids=None):
-        pixel_masks_all = []
-        if roi_ids is None:
-            roi_ids = np.array(self._all_roi_ids)
-        else:
-            roi_ids = np.array(roi_ids)
-        seg_id = np.array([self._roi_map[roi_id]['segmentation_id'] for roi_id in roi_ids])
-        roi_id_segmentation = np.array([self._roi_map[roi_id]['roi_id'] for roi_id in roi_ids])
-        for i in np.unique(seg_id):
-            seg_roi_ids = roi_id_segmentation[seg_id == i]
-            pixel_masks_all.extend(self._segmentations[i].get_roi_pixel_masks(roi_ids=seg_roi_ids))
-        return pixel_masks_all
+        return lambda x: [j for i in x for j in i]
 
+    @concatenate_output
     def get_roi_image_masks(self, roi_ids=None):
-        images_all = []
-        if roi_ids is None:
-            roi_ids = np.array(self._all_roi_ids)
-        else:
-            roi_ids = np.array(roi_ids)
-        seg_id = np.array([self._roi_map[roi_id]['segmentation_id'] for roi_id in roi_ids])
-        roi_id_segmentation = np.array([self._roi_map[roi_id]['roi_id'] for roi_id in roi_ids])
-        for i in np.unique(seg_id):
-            seg_roi_ids = roi_id_segmentation[seg_id == i]
-            images_all.append(self._segmentations[i].get_roi_image_masks(roi_ids=seg_roi_ids))
-        return np.concatenate(images_all, axis=2)
+        return lambda x: np.concatenate(x, axis=2)
 
     def get_roi_ids(self):
         return self._all_roi_ids
 
+    @concatenate_output
     def get_roi_locations(self, roi_ids=None):
-        roi_locations_all = []
-        if roi_ids is None:
-            roi_ids = np.array(self._all_roi_ids)
-        else:
-            roi_ids = np.array(roi_ids)
-        seg_id = np.array([self._roi_map[roi_id]['segmentation_id'] for roi_id in roi_ids])
-        roi_id_segmentation = np.array([self._roi_map[roi_id]['roi_id'] for roi_id in roi_ids])
-        for i in np.unique(seg_id):
-            seg_roi_ids = roi_id_segmentation[seg_id == i]
-            roi_locations_all.append(self._segmentations[i].get_roi_locations(roi_ids=seg_roi_ids))
-        return np.concatenate(roi_locations_all, axis=1)
+        return lambda x: np.concatenate(x, axis=1)
 
     def get_num_frames(self):
         return np.sum([self._segmentations[i].get_num_frames() for i in range(self._no_planes)])
@@ -172,11 +151,11 @@ class MultiSegmentationExtractor(SegmentationExtractor):
         multisegmentation_extractor: MultiSegmentationExtractor
         savepath: str
         kwargs: dict
-            example 'to_type': 'Suite2pSegmentationExtractor'
+            example 'to_type': Suite2pSegmentationExtractor
         """
         if kwargs.get('to_type'):
             try:
-                eval('to_type='+kwargs['to_type'])
+                to_type = kwargs['to_type']
             except:
                 raise Exception('enter a valid SegmentationExtractor type')
         else:
