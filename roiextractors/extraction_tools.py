38d94952a37af3b989a81bd58d47b7f001d896e2
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-09-08 11:32:43 -0400

    Merge branch 'nwbwriter-fix#2' of /Users/bendichter/dev/roiextractors with conflicts.

--- roiextractors/extraction_tools.py
+++ roiextractors/extraction_tools.py
@@ -1,6 +1,8 @@
 import numpy as np
 from typing import Union
 from pathlib import Path
+from functools import wraps
+from spikeextractors.extraction_tools import cast_start_end_frame
 
 ArrayType = Union[list, np.array]
 PathType = Union[str, Path]
@@ -8,26 +10,43 @@ NumpyArray = Union[np.array, np.memmap]
 DtypeType = [str, np.dtype]
 
 
-def _pixel_mask_extractor(_raw_images_trans, _roi_idx):
+def _pixel_mask_extractor(image_mask_, _roi_ids):
     '''An alternative data format for storage of image masks.
-
     Returns
     -------
     pixel_mask: numpy array
         Total pixels X 4 size. Col 1 and 2 are x and y location of the mask
         pixel, Col 3 is the weight of that pixel, Col 4 is the ROI index.
     '''
-    temp = np.empty((1, 4))
-    for i, roiid in enumerate(_roi_idx):
-        _np_raw_images_trans = np.array(_raw_images_trans[:, :, i])
-        _locs = np.where(_np_raw_images_trans > 0)
-        _pix_values = _np_raw_images_trans[_np_raw_images_trans > 0]
-        temp = np.append(temp, np.concatenate(
-            (_locs[0].reshape([1, np.size(_locs[0])]),
-             _locs[1].reshape([1, np.size(_locs[1])]),
-             _pix_values.reshape([1, np.size(_locs[1])]),
-             roiid * np.ones([1, np.size(_locs[1])]))).T, axis=0)
-    return temp[1::, :]
+    pixel_mask_list = []
+    for i, roiid in enumerate(_roi_ids):
+        image_mask = np.array(image_mask_[:, :, i])
+        _locs = np.where(image_mask > 0)
+        _pix_values = image_mask[image_mask > 0]
+        pixel_mask_list.append(np.vstack((_locs[0], _locs[1], _pix_values)).T)
+    return pixel_mask_list
+
+
+def _image_mask_extractor(pixel_mask, _roi_ids, image_shape):
+    """
+    Converts a pixel mask to image mask
+    
+    Parameters
+    ----------
+    pixel_mask: list
+        list of pixel masks (no pixels X 3)
+    _roi_ids: list
+    image_shape: array_like
+    
+    Returns
+    -------
+    image_mask: np.ndarray
+    """
+    image_mask = np.zeros(list(image_shape)+[len(_roi_ids)])
+    for no, rois in enumerate(_roi_ids):
+        for x, y, wt in pixel_mask[rois]:
+            image_mask[int(x), int(y), no] = wt
+    return image_mask
 
 
 def get_video_shape(video):
@@ -39,3 +58,49 @@ def get_video_shape(video):
         num_channels, num_frames, size_x, size_y = video.shape
 
     return num_channels, num_frames, size_x, size_y
+
+
+def check_get_frames_args(func):
+    @wraps(func)
+    def corrected_args(imaging, frame_idxs, channel=0):
+        channel = int(channel)
+        if isinstance(frame_idxs, (int, np.integer)):
+            frame_idxs = [frame_idxs]
+
+        if not isinstance(frame_idxs, slice):
+            frame_idxs = np.array(frame_idxs)
+        assert np.all(frame_idxs < imaging.get_num_frames()), "'frame_idxs' exceed number of frames"
+        get_frames_correct_arg = func(imaging, frame_idxs, channel)
+
+        if len(frame_idxs) == 1:
+            return get_frames_correct_arg[0]
+        else:
+            return get_frames_correct_arg
+    return corrected_args
+
+
+def check_get_videos_args(func):
+    @wraps(func)
+    def corrected_args(imaging, start_frame=None, end_frame=None, channel=0):
+        if start_frame is not None:
+            if start_frame > imaging.get_num_frames():
+                raise Exception(f"'start_frame' exceeds number of frames {imaging.get_num_frames()}!")
+            elif start_frame < 0:
+                start_frame = imaging.get_num_frames() + start_frame
+        else:
+            start_frame = 0
+        if end_frame is not None:
+            if end_frame > imaging.get_num_frames():
+                raise Exception(f"'end_frame' exceeds number of frames {imaging.get_num_frames()}!")
+            elif end_frame < 0:
+                end_frame = imaging.get_num_frames() + end_frame
+        else:
+            end_frame = imaging.get_num_frames()
+        assert end_frame - start_frame > 0, "'start_frame' must be less than 'end_frame'!"
+
+        start_frame, end_frame = cast_start_end_frame(start_frame, end_frame)
+        channel = int(channel)
+        get_videos_correct_arg = func(imaging, start_frame=start_frame, end_frame=end_frame, channel=channel)
+
+        return get_videos_correct_arg
+    return corrected_args
