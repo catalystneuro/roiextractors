814bb90d5986fc554beb31ec1b654c7d06e37f5d
Saksham Sharda <saksham20.sharda@gmail.com>
2020-09-10 12:27:22 -0500

    init skeleton

---
+++ roiextractors/tests/utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+import shutil
+import os
+from pathlib import Path
+
+
+def check_segmentations_equal(seg1,seg2):
+    pass
+
+def check_segmentation_return_types(seg):
+    pass
+
+def check_write_segmentation(seg):
+    pass
