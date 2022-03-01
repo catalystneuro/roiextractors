420cb9172aac334560cd1f8f1c8c7533bd9958f7
Cody Baker <codycbakerphd@gmail.com>
2022-03-01 16:43:52 -0500

    renamed testing file

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,13 +1,11 @@
 import tempfile
 import unittest
-import numpy as np
 from pathlib import Path
 
 from datalad.api import install, Dataset
 from parameterized import parameterized
 
 from roiextractors import (
-    NwbSegmentationExtractor,
     CaimanSegmentationExtractor,
     ExtractSegmentationExtractor,
     Suite2pSegmentationExtractor,
@@ -15,9 +13,7 @@ from roiextractors import (
     TiffImagingExtractor,
     Hdf5ImagingExtractor,
     SbxImagingExtractor,
-    NwbImagingExtractor,
 )
-from roiextractors.example_datasets import toy_example
 from roiextractors.testing import check_segmentations_equal, check_imaging_equal
 
 
@@ -38,7 +34,7 @@ class TestNwbConversions(unittest.TestCase):
         save_path = self.savedir / save_fname
         rt_write_path = self.savedir / rt_write_fname
         rt_read_path = self.savedir / rt_read_fname
-        resp = self.dataset.get(dataset_path)
+        _ = self.dataset.get(dataset_path)
 
         return rt_write_path, rt_read_path, save_path
 
@@ -75,7 +71,7 @@ class TestNwbConversions(unittest.TestCase):
             ),
         ]
     )
-    def test_convert_seg_interface_to_nwb(
+    def test_seg_interfaces(
         self,
         roi_ex_class,
         dataset_path,
@@ -93,9 +89,10 @@ class TestNwbConversions(unittest.TestCase):
         roi_ex = roi_ex_class(path)
         try:
             roi_ex_class.write_segmentation(roi_ex, rt_write_path)
+            roi_ex_rt = roi_ex_class(rt_read_path)
+            check_segmentations_equal(seg1=roi_ex, seg2=roi_ex_rt)
         except NotImplementedError:
             return
-        seg_ex_rt = roi_ex_class(rt_read_path)
 
     @parameterized.expand(
         [
@@ -122,7 +119,7 @@ class TestNwbConversions(unittest.TestCase):
             ),
         ]
     )
-    def test_convert_img_interface_to_nwb(
+    def test_img_interfaces(
         self,
         roi_ex_class,
         dataset_path,
@@ -142,19 +139,12 @@ class TestNwbConversions(unittest.TestCase):
         roi_ex = roi_ex_class(file_path=path, sampling_frequency=sampling_freq)
         try:
             roi_ex_class.write_imaging(roi_ex, rt_write_path)
+            img_ex_rt = roi_ex_class(
+                file_path=rt_read_path, sampling_frequency=sampling_freq
+            )
+            check_imaging_equal(img1=roi_ex, img2=img_ex_rt)
         except NotImplementedError:
             return
-        img_ex_rt = roi_ex_class(
-            file_path=rt_read_path, sampling_frequency=sampling_freq
-        )
-
-    def test_use_times(self):
-        imag, _ = toy_example(duration=10, num_rois=2)
-
-        timestamps = imag.frame_to_time(np.arange(imag.get_num_frames())) + 0.5
-        imag.set_times(timestamps)
-
-        save_path = "timestamps.nwb"
 
 
 if __name__ == "__main__":
