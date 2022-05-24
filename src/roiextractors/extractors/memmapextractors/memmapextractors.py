e232ce24420d2605e486f62986ce4fb346855ca8
h-mayorquin <h.mayorquin@gmail.com>
2022-05-24 16:20:59 -0700

    eliminate the __init__

--- src/roiextractors/extractors/memmapextractors/memmapextractors.py
+++ src/roiextractors/extractors/memmapextractors/memmapextractors.py
@@ -22,84 +22,12 @@ class MemmapImagingExtractor(ImagingExtractor):
 
     def __init__(
         self,
-        file_path: PathType,
-        frame_shape: Tuple[int, int],
-        sampling_frequency: float,
-        dtype: DtypeType,
-        offset: int = 0,
-        image_structure_to_axis: Dict[str, int] = None,
+        imaging_extractor: ImagingExtractor,
+        save_path: PathType = None,
+        verbose: bool = False,
     ):
-        """Class for reading optical imaging data stored in a binary format
 
-
-        Parameters
-        ----------
-        file_path : PathType
-            the file_path where the data resides.
-        frame_shape : tuple
-            The frame shape of the image determines how each frame looks. Examples:
-            (n_channels, rows, columns), (rows, columns, n_channels), (n_channels, columns, rows), etc.
-            Note that n_channels is 1 for grayscale and 3 for color images.
-        dtype : DtypeType
-            The type of the data to be loaded (int, float, etc.)
-        offset : int, optional
-            The offset in bytes. Usually corresponds to the number of bytes occupied by the header. 0 by default.
-        sampling_frequency : float, optional
-            The sampling frequency.
-        image_structure_to_axis : dict, optional
-            A dictionary indicating what axis corresponds to what in the memmap. The default values are:
-            dict(frame_axis=0, num_channels=1, rows=2, columns=3)
-            frame_axis=0 indicates that the first axis corresponds to the frames (usually time)
-            num_channels=1 here indicates that the first axis corresponds to the  n_channels.
-            rows=2 indicates that the rows in the image are in the second axis
-            columns=3 indicates that columns is the last axis or dimension in this structure.
-
-            Notice that this should correspond with frame_shape.
-        """
-
-        self.installed = True
-        super().__init__()
-
-        self.file_path = Path(file_path)
-        self._sampling_frequency = sampling_frequency
-        self.offset = offset
-        self.dtype = dtype
-
-        # Get the structure, apply default if not available
-        self.frame_shape = frame_shape
-        image_structure_to_axis = dict() if image_structure_to_axis is None else image_structure_to_axis
-        self.image_structure_to_axis = dict(frame_axis=0, num_channels=1, rows=2, columns=3)
-        self.image_structure_to_axis.update(image_structure_to_axis)
-        self.frame_axis = self.image_structure_to_axis["frame_axis"]
-
-        # Extract video
-        self._video = self.read_binary_video()
-
-        # Get the image structure as attributes
-        self._rows = self._video.shape[self.image_structure_to_axis["rows"]]
-        self._columns = self._video.shape[self.image_structure_to_axis["columns"]]
-        self._num_channels = self._video.shape[self.image_structure_to_axis["num_channels"]]
-        self._num_frames = self._video.shape[self.image_structure_to_axis["frame_axis"]]
-
-    def read_binary_video(self):
-        file = self.file_path.open()
-        file_descriptor = file.fileno()
-        file_size_bytes = os.fstat(file_descriptor).st_size
-
-        pixels_per_frame = np.prod(self.frame_shape)
-        type_size = np.dtype(self.dtype).itemsize
-        frame_size_bytes = pixels_per_frame * type_size
-
-        bytes_available = file_size_bytes - self.offset
-        number_of_frames = bytes_available // frame_size_bytes
-
-        memmap_shape = list(self.frame_shape)
-        memmap_shape.insert(self.frame_axis, number_of_frames)
-        memmap_shape = tuple(memmap_shape)
-
-        video_memap = np.memmap(self.file_path, offset=self.offset, dtype=self.dtype, mode="r", shape=memmap_shape)
-
-        return video_memap
+        pass
 
     def get_frames(self, frame_idxs=None):
         if frame_idxs is None:
@@ -154,8 +82,8 @@ class MemmapImagingExtractor(ImagingExtractor):
         video_to_save = np.memmap(
             save_path,
             shape=(
-                imaging.get_num_channels(),
                 imaging.get_num_frames(),
+                imaging.get_num_channels(),
                 imaging.get_image_size()[0],
                 imaging.get_image_size()[1],
             ),
