911db29debc71ee2e936fe3d9b759c7f17cce4c3
Cody Baker <cbaker9@nd.edu>
2022-06-03 15:41:01 -0400

    filled in more tests

--- tests/test_internals/test_frame_slice_imaging.py
+++ tests/test_internals/test_frame_slice_imaging.py
@@ -1,15 +1,43 @@
 import unittest
 
-import numnpy as np
+from hdmf.testing import TestCase
+import numpy as np
+from numpy.testing import assert_array_equal
 
 from roiextractors import NumpyImagingExtractor
 
 
-class TestFrameSliceImaging(unittest.TestCase):
+class TestFrameSliceImaging(TestCase):
     @classmethod
     def setUpClass(cls):
         """Use a toy example of ten frames of a 5 x 4 grayscale image."""
-        cls.toy_example = NumpyImagingExtractor(timeseries=np.random.random(size=[10, 5, 4, 1]), sampling_frequency=1.0)
+        cls.toy_data = np.random.random(size=[10, 5, 4, 1])
+        cls.toy_imaging_example = NumpyImagingExtractor(timeseries=cls.toy_data, sampling_frequency=1.0)
+        cls.frame_sliced_imaging = cls.toy_imaging_example.frame_slice(start_frame=2, end_frame=7)
+
+    def test_get_image_size(self):
+        assert self.frame_sliced_imaging.get_image_size() == (5, 4)
+
+    def test_get_num_frames(self):
+        assert self.frame_sliced_imaging.get_num_frames() == 5
+
+    def test_get_sampling_frequency(self):
+        assert self.frame_sliced_imaging.get_sampling_frequency() == 1.0
+
+    def test_get_channel_names(self):
+        assert self.frame_sliced_imaging.get_channel_names() is None
+
+    def test_get_num_channels(self):
+        assert self.frame_sliced_imaging.get_num_channels() == 1
+
+    def test_get_frames_assertion(self):
+        with self.assertRaisesWith(
+            exc_type=AssertionError, exc_msg="'frame_idxs' range beyond number of available frames!"
+        ):
+            self.frame_sliced_imaging.get_frames(frame_idxs=[6])
+
+    def test_get_frames(self):
+        assert_array_equal(self.frame_sliced_imaging.get_frames(frame_idxs=[2, 4]), self.toy_data[[4, 6], ...])
 
 
 if __name__ == "__main__":
