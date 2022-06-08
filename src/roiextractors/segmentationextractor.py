c9a047f753e9c59a17db5e1becdc5a804116d3a8
Cody Baker <cbaker9@nd.edu>
2022-06-08 17:02:44 -0400

    wip

--- src/roiextractors/segmentationextractor.py
+++ src/roiextractors/segmentationextractor.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Optional, Tuple
 
 import numpy as np
 from spikeextractors.baseextractor import BaseExtractor
@@ -273,6 +274,10 @@ class SegmentationExtractor(ABC, BaseExtractor):
         """
         return self._num_planes
 
+    def frame_slice(self, start_frame, end_frame):
+        """Return a new ImagingExtractor ranging from the start_frame to the end_frame."""
+        return FrameSliceSegmentationExtractor(parent_imaging=self, start_frame=start_frame, end_frame=end_frame)
+
     @staticmethod
     def write_segmentation(segmentation_extractor, save_path, overwrite=False):
         """
@@ -289,3 +294,60 @@ class SegmentationExtractor(ABC, BaseExtractor):
             If True, the file is overwritten if existing (default False)
         """
         raise NotImplementedError
+
+
+class FrameSliceSegmentationExtractor(SegmentationExtractor):
+    """
+    Class to get a lazy frame slice.
+
+    Do not use this class directly but use `.frame_slice(...)`
+    """
+
+    extractor_name = "FrameSliceImagingExtractor"
+    installed = True
+    is_writable = True
+    installation_mesg = ""
+
+    def __init__(
+        self,
+        parent_segmentation: SegmentationExtractor,
+        start_frame: Optional[int] = None,
+        end_frame: Optional[int] = None,
+    ):
+        self._parent_segmentation = parent_segmentation
+        self._start_frame = start_frame
+        self._end_frame = end_frame
+        self._num_frames = self._end_frame - self._start_frame
+
+        parent_size = self._parent_imaging.get_num_frames()
+        if start_frame is None:
+            start_frame = 0
+        else:
+            assert 0 <= start_frame < parent_size
+        if end_frame is None:
+            end_frame = parent_size
+        else:
+            assert 0 < end_frame <= parent_size
+        assert end_frame > start_frame, "'start_frame' must be smaller than 'end_frame'!"
+
+        super().__init__()
+
+    def get_frames(self, frame_idxs: ArrayType, channel: int = 0) -> np.ndarray:
+        assert max(frame_idxs) < self._num_frames, "'frame_idxs' range beyond number of available frames!"
+        mapped_frame_idxs = np.array(frame_idxs) + self._start_frame
+        return self._parent_imaging.get_frames(frame_idxs=mapped_frame_idxs, channel=channel)
+
+    def get_image_size(self) -> Tuple[int, int]:
+        return tuple(self._parent_imaging.get_image_size())
+
+    def get_num_frames(self) -> int:
+        return self._num_frames
+
+    def get_sampling_frequency(self) -> float:
+        return self._parent_imaging.get_sampling_frequency()
+
+    def get_channel_names(self) -> list:
+        return self._parent_imaging.get_channel_names()
+
+    def get_num_channels(self) -> int:
+        return self._parent_imaging.get_num_channels()
---
+++ tests/test_internals/test_frame_slicing_segmentation.py
@@ -0,0 +1,46 @@
+import unittest
+
+from hdmf.testing import TestCase
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from roiextractors import NumpySegmentationExtractor
+
+
+class TestFrameSliceSegmentation(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Use a toy example of ten frames of a 5 x 4 grayscale image."""
+        cls.toy_data = np.random.random(size=(1, 10, 5, 4))
+        cls.toy_imaging_example = NumpySegmentationExtractor(timeseries=cls.toy_data, sampling_frequency=1.0)
+        cls.frame_sliced_segmentation = cls.toy_imaging_example.frame_slice(start_frame=2, end_frame=7)
+
+    def test_get_image_size(self):
+        assert self.frame_sliced_segmentation.get_image_size() == (5, 4)
+
+    def test_get_num_frames(self):
+        assert self.frame_sliced_segmentation.get_num_frames() == 5
+
+    def test_get_sampling_frequency(self):
+        assert self.frame_sliced_segmentation.get_sampling_frequency() == 1.0
+
+    def test_get_channel_names(self):
+        assert self.frame_sliced_segmentation.get_channel_names() == ["channel_0"]
+
+    def test_get_num_channels(self):
+        assert self.frame_sliced_segmentation.get_num_channels() == 1
+
+    def test_get_frames_assertion(self):
+        with self.assertRaisesWith(
+            exc_type=AssertionError, exc_msg="'frame_idxs' range beyond number of available frames!"
+        ):
+            self.frame_sliced_segmentation.get_frames(frame_idxs=[6])
+
+    def test_get_frames(self):
+        assert_array_equal(
+            self.frame_sliced_segmentation.get_frames(frame_idxs=[2, 4]), self.toy_data[:, [4, 6], ...].squeeze()
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
