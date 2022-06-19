69434294f8b0784958e49fb62b6999ea4d24d1ee
Cody Baker <codycbakerphd@gmail.com>
2022-06-19 17:59:36 -0400

    debug

--- src/roiextractors/extractors/tiffimagingextractors/scanimagetiffimagingextractor.py
+++ src/roiextractors/extractors/tiffimagingextractors/scanimagetiffimagingextractor.py
@@ -39,7 +39,9 @@ class ScanImageTiffImagingExtractor(ImagingExtractor):
         """
         Specialized extractor for reading TIFF files produced via ScanImage.
 
-        This extractor allows for lazy accessing of slices, unlike :py:class:`~roiextractors.extractors.tiffimagingextractors.TiffImagingExtractor`. However, direct slicing of the underlying data structure is not equivalent to a numpy memory map.
+        This extractor allows for lazy accessing of slices, unlike
+        :py:class:`~roiextractors.extractors.tiffimagingextractors.TiffImagingExtractor`.
+        However, direct slicing of the underlying data structure is not equivalent to a numpy memory map.
 
         Parameters
         ----------
@@ -99,3 +101,6 @@ class ScanImageTiffImagingExtractor(ImagingExtractor):
 
     def get_num_channels(self) -> int:
         return self._num_channels
+
+    def get_channel_names(self) -> list:
+        pass
