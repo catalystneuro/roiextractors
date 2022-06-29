4531cf0623dcb60d8f7ce7495d50b3d7796b7a5e
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-06-29 10:06:01 -0400

    v0.4.15
    v0.4.15 - debugged dtype retrieval for numpy arrays

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if not gin_config_file_local.exists():
 
 setup(
     name="roiextractors",
-    version="0.4.14",
+    version="0.4.15",
     author="Heberto Mayorquin, Cody Baker, Ben Dichter, Alessio Buccino",
     author_email="ben.dichter@gmail.com",
     description="Python module for extracting optical physiology ROIs and traces for various file types and formats",
