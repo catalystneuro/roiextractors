0eeab92dce6776a2f54e44c9a84c58c226ec94a6
Szonja Weigl <weigl.anna.szonja@hallgato.ppke.hu>
2022-08-04 14:48:05 +0200

    Update tests/test_internals/test_multiimagingextractor.py
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- tests/test_internals/test_multiimagingextractor.py
+++ tests/test_internals/test_multiimagingextractor.py
@@ -80,7 +80,7 @@ class TestMultiImagingExtractor(TestCase):
         )
         assert_array_equal(test_frames, expected_frames)
 
-    def test_get_get_video_single_frame(self):
+    def test_get_video_single_frame(self):
         test_frames = self.multi_imaging_extractor.get_video(start_frame=10, end_frame=11)
         expected_frames = self.extractors[1].get_video(start_frame=0, end_frame=1)
 
