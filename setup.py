ae5f25203e6ac5b1402bad2fac4e65d9e0fecd15
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-05-04 11:27:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ if not gin_config_file_local.exists():
 
 setup(
     name="roiextractors",
-    version="0.5.2",
-    author="Heberto Mayorquin, Cody Baker, Ben Dichter, Alessio Buccino",
+    version="0.5.3",
+    author="Heberto Mayorquin, Szonja Weigl, Cody Baker, Ben Dichter, Alessio Buccino",
     author_email="ben.dichter@gmail.com",
     description="Python module for extracting optical physiology ROIs and traces for various file types and formats",
     url="https://github.com/catalystneuro/roiextractors",
