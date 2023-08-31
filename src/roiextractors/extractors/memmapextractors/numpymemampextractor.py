0d8e86f91909481444c0d59238c3954d211896c4
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-08-31 13:34:43 -0700

    added docstrings to memmap

--- src/roiextractors/extractors/memmapextractors/numpymemampextractor.py
+++ src/roiextractors/extractors/memmapextractors/numpymemampextractor.py
@@ -1,3 +1,10 @@
+"""NumpyMemmapImagingExtractor class.
+
+Classes
+-------
+NumpyMemmapImagingExtractor
+    The class for reading optical imaging data stored in a binary format with numpy.memmap.
+"""
 import os
 from pathlib import Path
 from typing import Tuple, Dict
@@ -12,6 +19,8 @@ from .memmapextractors import MemmapImagingExtractor
 
 
 class NumpyMemmapImagingExtractor(MemmapImagingExtractor):
+    """An ImagingExtractor class for reading optical imaging data stored in a binary format with numpy.memmap."""
+
     extractor_name = "NumpyMemmapImagingExtractor"
 
     def __init__(
@@ -22,8 +31,7 @@ class NumpyMemmapImagingExtractor(MemmapImagingExtractor):
         dtype: DtypeType,
         offset: int = 0,
     ):
-        """Class for reading optical imaging data stored in a binary format with np.memmap
-
+        """Create an instance of NumpyMemmapImagingExtractor.
 
         Parameters
         ----------
@@ -63,7 +71,6 @@ class NumpyMemmapImagingExtractor(MemmapImagingExtractor):
         offset : int, optional
             The offset in bytes. Usually corresponds to the number of bytes occupied by the header. 0 by default.
         """
-
         self.installed = True
 
         self.file_path = Path(file_path)
