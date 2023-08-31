e574d8f98fb28d9383b184036bb1438cf9de447d
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-08-31 16:14:20 -0700

    added docstrings to scanbox image

--- src/roiextractors/extractors/sbximagingextractor/__init__.py
+++ src/roiextractors/extractors/sbximagingextractor/__init__.py
@@ -1,1 +1,13 @@
+"""A segmentation extractor for Scanbox imaging data.
+
+Modules
+-------
+sbximagingextractor
+    A segmentation extractor for Scanbox imaging data.
+
+Classes
+-------
+SbxImagingExtractor
+    An ImagingExtractor for Scanbox Image files.
+"""
 from .sbximagingextractor import SbxImagingExtractor
