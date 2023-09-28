f6d1896f16194fb284251d5d534d4f996160adb2
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-09-28 16:05:30 -0700

    added tests for the rest of the volumetric extractor methods

--- tests/test_volumetricimagingextractor.py
+++ tests/test_volumetricimagingextractor.py
@@ -66,3 +66,56 @@ def test_get_frames(volumetric_imaging_extractor, frame_idxs):
 def test_get_frames_invalid(volumetric_imaging_extractor, frame_idxs):
     with pytest.raises(ValueError):
         volumetric_imaging_extractor.get_frames(frame_idxs=frame_idxs)
+
+
+@pytest.mark.parametrize("num_rows, num_columns, num_planes", [(1, 2, 3), (2, 1, 3), (3, 2, 1)])
+def test_get_image_size(num_rows, num_columns, num_planes):
+    imaging_extractors = [
+        generate_dummy_imaging_extractor(num_rows=num_rows, num_columns=num_columns) for _ in range(num_planes)
+    ]
+    volumetric_imaging_extractor = VolumetricImagingExtractor(imaging_extractors=imaging_extractors)
+    assert volumetric_imaging_extractor.get_image_size() == (num_rows, num_columns, num_planes)
+
+
+@pytest.mark.parametrize("num_planes", [1, 2, 3])
+def test_get_num_planes(num_planes):
+    imaging_extractors = [generate_dummy_imaging_extractor() for _ in range(num_planes)]
+    volumetric_imaging_extractor = VolumetricImagingExtractor(imaging_extractors=imaging_extractors)
+    assert volumetric_imaging_extractor.get_num_planes() == num_planes
+
+
+@pytest.mark.parametrize("num_frames", [1, 2, 3])
+def test_get_num_frames(num_frames):
+    imaging_extractors = [generate_dummy_imaging_extractor(num_frames=num_frames)]
+    volumetric_imaging_extractor = VolumetricImagingExtractor(imaging_extractors=imaging_extractors)
+    assert volumetric_imaging_extractor.get_num_frames() == num_frames
+
+
+@pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
+def test_get_sampling_frequency(sampling_frequency):
+    imaging_extractors = [generate_dummy_imaging_extractor(sampling_frequency=sampling_frequency)]
+    volumetric_imaging_extractor = VolumetricImagingExtractor(imaging_extractors=imaging_extractors)
+    assert volumetric_imaging_extractor.get_sampling_frequency() == sampling_frequency
+
+
+@pytest.mark.parametrize("channel_names", [["Channel 1"], [" Channel 1 ", "Channel 2"]])
+def test_get_channel_names(channel_names):
+    imaging_extractors = [
+        generate_dummy_imaging_extractor(channel_names=channel_names, num_channels=len(channel_names))
+    ]
+    volumetric_imaging_extractor = VolumetricImagingExtractor(imaging_extractors=imaging_extractors)
+    assert volumetric_imaging_extractor.get_channel_names() == channel_names
+
+
+@pytest.mark.parametrize("num_channels", [1, 2, 3])
+def test_get_num_channels(num_channels):
+    imaging_extractors = [generate_dummy_imaging_extractor(num_channels=num_channels)]
+    volumetric_imaging_extractor = VolumetricImagingExtractor(imaging_extractors=imaging_extractors)
+    assert volumetric_imaging_extractor.get_num_channels() == num_channels
+
+
+@pytest.mark.parametrize("dtype", [np.float64, np.int16, np.uint8])
+def test_get_dtype(dtype):
+    imaging_extractors = [generate_dummy_imaging_extractor(dtype=dtype)]
+    volumetric_imaging_extractor = VolumetricImagingExtractor(imaging_extractors=imaging_extractors)
+    assert volumetric_imaging_extractor.get_dtype() == dtype
