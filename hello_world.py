e998bcbff0117467188a3a9f5a40bd150dec3dd0
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-10-02 11:05:11 +0200

    aider: Refactor hello_world function to include a docstring.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def hello_world():
-	print("Hello World!")
-	
+    """
+    Prints "Hello World!" to the console.
+
+    This function does not take any arguments and does not return anything.
+    """
+    print("Hello World!")
+
 if __name__ == '__main__':
-	hello_world() 
+    hello_world()
