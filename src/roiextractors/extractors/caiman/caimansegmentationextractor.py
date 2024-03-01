76fec6f05409153ba48a6e547b5a1ca59905874e
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2024-03-01 17:50:56 +0100

    correct b with f for neuropil components

--- src/roiextractors/extractors/caiman/caimansegmentationextractor.py
+++ src/roiextractors/extractors/caiman/caimansegmentationextractor.py
@@ -57,7 +57,7 @@ class CaimanSegmentationExtractor(SegmentationExtractor):
         self._dataset_file = self._file_extractor_read()
         self._roi_response_dff = self._trace_extractor_read("F_dff")
         self._roi_response_denoised = self._trace_extractor_read("C")
-        self._roi_response_neuropil = self._trace_extractor_read("b")
+        self._roi_response_neuropil = self._trace_extractor_read("f")
         self._roi_response_deconvolved = self._trace_extractor_read("S")
         self._image_correlation = self._summary_image_read()
         self._sampling_frequency = self._dataset_file["params"]["data"]["fr"][()]
@@ -181,7 +181,7 @@ class CaimanSegmentationExtractor(SegmentationExtractor):
             if segmentation_object.get_traces(name="denoised") is not None:
                 estimates.create_dataset("C", data=segmentation_object.get_traces(name="denoised"))
             if segmentation_object.get_traces(name="neuropil") is not None:
-                estimates.create_dataset("b", data=segmentation_object.get_traces(name="neuropil"))
+                estimates.create_dataset("f", data=segmentation_object.get_traces(name="neuropil"))
             if segmentation_object.get_traces(name="dff") is not None:
                 estimates.create_dataset("F_dff", data=segmentation_object.get_traces(name="dff"))
             if segmentation_object.get_traces(name="deconvolved") is not None:
