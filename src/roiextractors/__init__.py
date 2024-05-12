199e55692e71551c35af1a9d68370dffd465a0ec
Ben Dichter <ben.dichter@gmail.com>
2024-05-12 18:30:43 -0500

    drop python 3.8

--- src/roiextractors/__init__.py
+++ src/roiextractors/__init__.py
@@ -1,16 +1,8 @@
 """Python-based module for extracting from, converting between, and handling recorded and optical imaging data from several file formats."""
 
-# Keeping __version__ accessible only to maintain backcompatability.
-# Modern approach (Python >= 3.8) is to use importlib
-try:
-    from importlib.metadata import version
+from importlib.metadata import version
 
-    __version__ = version("roiextractors")
-except ModuleNotFoundError:  # Remove the except clause when minimal supported version becomes 3.8
-    from pkg_resources import get_distribution
-
-    __version__ = get_distribution("roiextractors").version
-
+__version__ = version("roiextractors")
 
 from .example_datasets import toy_example
 from .extraction_tools import show_video
