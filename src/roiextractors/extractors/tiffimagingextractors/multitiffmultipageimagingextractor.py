fb772d6926b72291ce10a042338610ba59275748
Ben Dichter <ben.dichter@gmail.com>
2024-05-15 18:02:08 -0500

    fix tests

--- src/roiextractors/extractors/tiffimagingextractors/multitiffmultipageimagingextractor.py
+++ src/roiextractors/extractors/tiffimagingextractors/multitiffmultipageimagingextractor.py
@@ -3,7 +3,7 @@ from typing import Optional, Tuple
 import numpy as np
 from tqdm import tqdm
 
-from ... import ImagingExtractor
+from ...imagingextractor import ImagingExtractor
 from ...extraction_tools import get_package
 from ...utils import match_paths
 
@@ -33,14 +33,15 @@ class MultiTiffMultiPageImagingExtractor(ImagingExtractor):
         self.tif_paths = match_paths(folder_path, pattern)
         self._tifffile = get_package(package_name="tifffile", installation_instructions="pip install tifffile")
 
-        self.page_tracker = []
+        page_tracker = []
         page_counter = 0
         for file_path in tqdm(self.tif_paths, "extracting page lengths"):
             with self._tifffile.TiffFile(file_path) as tif:
-                self.page_tracker.append(page_counter)
+                page_tracker.append(page_counter)
                 page_counter += len(tif.pages)
+        self.page_tracker = np.array(page_tracker)
 
-        page = next(tif.pages)
+        page = tif.pages[0]
 
         self._num_frames = page_counter
         self._num_columns = page.imagewidth
@@ -52,7 +53,8 @@ class MultiTiffMultiPageImagingExtractor(ImagingExtractor):
 
     def get_video(self, start_frame: int = None, end_frame: int = None, channel: Optional[int] = 0) -> np.ndarray:
         frame_idxs = np.arange(start_frame or 0, end_frame or self._num_frames)
-        file_idxs = np.searchsorted(self.page_tracker, frame_idxs)  # index of the file that contains the frame
+        file_idxs = np.searchsorted(self.page_tracker, frame_idxs, side="right") - 1  # index of the file that contains the frame
+        print(f"{file_idxs=}")
         file_start_idxs = self.page_tracker[file_idxs]  # index of the first frame in the file
         frame_offset_idxs = frame_idxs - file_start_idxs  # index of the frame in the file
         # dict of file_idx: frame_offset_idxs
@@ -60,7 +62,7 @@ class MultiTiffMultiPageImagingExtractor(ImagingExtractor):
 
         data = []
         for file_idx, frame_offset_idxs in index_dict.items():
-            with self._tifffile.TiffFile(self.tif_paths[file_idx]) as tif:
+            with self._tifffile.TiffFile(list(self.tif_paths)[file_idx]) as tif:
                 for frame_offset_idx in frame_offset_idxs:
                     page = tif.pages[frame_offset_idx]
                     data.append(page.asarray())
--- tests/test_multitiff_multipage_imaging_extractor.py
+++ tests/test_multitiff_multipage_imaging_extractor.py
@@ -1,9 +1,12 @@
 from roiextractors import MultiTiffMultiPageImagingExtractor
 
+from tests.setup_paths import OPHYS_DATA_PATH
+
 
 def test_init_multitiff_multipage_imaging_extractor():
     extractor = MultiTiffMultiPageImagingExtractor(
-        folder_path="tests/test_files", pattern="split_{:d}.tif", sampling_frequency=1.0
+        folder_path=OPHYS_DATA_PATH / "imaging_datasets" / "Tif" / "splits", pattern="split_{:d}.tif",
+        sampling_frequency=1.0
     )
 
     assert extractor.get_num_channels() == 1
@@ -11,5 +14,5 @@ def test_init_multitiff_multipage_imaging_extractor():
     assert extractor.get_sampling_frequency() == 1.0
     assert extractor.get_channel_names() == ["channel_0"]
     assert extractor.get_dtype() == "uint16"
-    assert extractor.get_image_size() == (512, 512)
-    assert extractor.get_video().shape == (2000, 512, 512)
+    assert extractor.get_image_size() == (60, 80)
+    assert extractor.get_video().shape == (2000, 60, 80)
