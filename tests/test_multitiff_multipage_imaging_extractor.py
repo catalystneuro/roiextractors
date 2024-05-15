7263bd93879e5c0ffc8b8a9340a795e37883ed4b
Ben Dichter <ben.dichter@gmail.com>
2024-05-15 18:28:28 -0500

    fix test

--- tests/test_multitiff_multipage_imaging_extractor.py
+++ tests/test_multitiff_multipage_imaging_extractor.py
@@ -5,7 +5,7 @@ from tests.setup_paths import OPHYS_DATA_PATH
 
 def test_init_multitiff_multipage_imaging_extractor():
     extractor = MultiTiffMultiPageImagingExtractor(
-        folder_path=OPHYS_DATA_PATH / "imaging_datasets" / "Tif" / "splits", pattern="split_{:d}.tif",
+        folder_path=OPHYS_DATA_PATH / "imaging_datasets" / "Tif" / "splits", pattern="split_{split:d}.tif",
         sampling_frequency=1.0
     )
 
@@ -16,3 +16,18 @@ def test_init_multitiff_multipage_imaging_extractor():
     assert extractor.get_dtype() == "uint16"
     assert extractor.get_image_size() == (60, 80)
     assert extractor.get_video().shape == (2000, 60, 80)
+    assert list(extractor.tif_paths.keys()) == [
+        str(OPHYS_DATA_PATH / "imaging_datasets" / "Tif" / "splits" / x) for x in
+        (
+            "split_1.tif",
+            "split_2.tif",
+            "split_3.tif",
+            "split_4.tif",
+            "split_5.tif",
+            "split_6.tif",
+            "split_7.tif",
+            "split_8.tif",
+            "split_9.tif",
+            "split_10.tif",
+         )
+    ]
