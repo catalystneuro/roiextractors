c1ae185dad16311d412f66b88a9528c39961a5e5
Ben Dichter <ben.dichter@gmail.com>
2024-05-15 19:48:16 -0500

    Add docstring for match_paths

--- src/roiextractors/utils.py
+++ src/roiextractors/utils.py
@@ -1,10 +1,27 @@
 import glob
 import os
+from typing import Dict, Any
 
 from parse import parse
 
 
-def match_paths(base, pattern, sort=True):
+def match_paths(base: str, pattern: str, sort=True) -> Dict[str, Dict[str, Any]]:
+    """
+    Match paths in a directory to a pattern.
+
+    Parameters
+    ----------
+    base: str
+        The base directory to search in.
+    pattern: str
+        The f-string pattern to match the paths to.
+    sort: bool, default=True
+        Whether to sort the output by the values of the named groups in the pattern.
+
+    Returns
+    -------
+    dict
+    """
     full_pattern = os.path.join(base, pattern)
     paths = glob.glob(os.path.join(base, "*"))
     out = {}
