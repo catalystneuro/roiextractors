0feba561bdb849d73d16b5bff8a78e4f19bc2aef
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-05-24 10:02:18 -0700

    private members = False

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ autodata_content = "both"
 autodoc_default_options = {
     "members": True,
     "member-order": "bysource",
-    "private-members": True,
+    "private-members": False,
     "show-inheritance": False,
     "toctree": True,
 }
