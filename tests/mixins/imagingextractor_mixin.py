cd26c3426c11bfe47409c8d64f8b7d9ca5013743
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-09-23 17:57:49 -0700

    refactored frame slice tests into their own mixin

--- tests/mixins/imagingextractor_mixin.py
+++ tests/mixins/imagingextractor_mixin.py
@@ -1,17 +1,8 @@
-from typing import Type
-from roiextractors import ImagingExtractor
 import pytest
 import numpy as np
-from roiextractors.extraction_tools import ArrayType
 
 
 class ImagingExtractorMixin:
-    imaging_extractor_cls: Type[ImagingExtractor]
-
-    @pytest.fixture(scope="class")
-    def imaging_extractor(self, imaging_extractor_kwargs):
-        return self.imaging_extractor_cls(**imaging_extractor_kwargs)
-
     def test_get_image_size(self, imaging_extractor, expected_video):
         image_size = imaging_extractor.get_image_size()
         assert image_size == (expected_video.shape[1], expected_video.shape[2])
@@ -70,23 +61,22 @@ class ImagingExtractorMixin:
             imaging_extractor.get_frames(frame_idxs=[0.5])
 
     @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
-    def test_frame_to_time_no_times(self, imaging_extractor_kwargs, sampling_frequency):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
+    def test_frame_to_time_no_times(self, imaging_extractor, sampling_frequency):
         imaging_extractor._times = None
         imaging_extractor._sampling_frequency = sampling_frequency
         times = imaging_extractor.frame_to_time(frames=[0, 1])
         expected_times = np.array([0, 1]) / sampling_frequency
         assert np.array_equal(times, expected_times)
 
-    def test_frame_to_time_with_times(self, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor._times = np.array([0, 1])
+    def test_frame_to_time_with_times(self, imaging_extractor):
+        expected_times = np.array([0, 1])
+        imaging_extractor._times = expected_times
         times = imaging_extractor.frame_to_time(frames=[0, 1])
-        assert np.array_equal(times, imaging_extractor._times)
+
+        assert np.array_equal(times, expected_times)
 
     @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
-    def test_time_to_frame_no_times(self, imaging_extractor_kwargs, sampling_frequency):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
+    def test_time_to_frame_no_times(self, imaging_extractor, sampling_frequency):
         imaging_extractor._times = None
         imaging_extractor._sampling_frequency = sampling_frequency
         times = np.array([0, 1]) / sampling_frequency
@@ -94,16 +84,14 @@ class ImagingExtractorMixin:
         expected_frames = np.array([0, 1])
         assert np.array_equal(frames, expected_frames)
 
-    def test_time_to_frame_with_times(self, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
+    def test_time_to_frame_with_times(self, imaging_extractor):
         imaging_extractor._times = np.array([0, 1])
         times = np.array([0, 1])
         frames = imaging_extractor.time_to_frame(times=times)
         expected_frames = np.array([0, 1])
         assert np.array_equal(frames, expected_frames)
 
-    def test_set_times(self, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
+    def test_set_times(self, imaging_extractor):
         times = np.arange(imaging_extractor.get_num_frames())
         imaging_extractor.set_times(times)
         assert np.array_equal(imaging_extractor._times, times)
@@ -113,22 +101,144 @@ class ImagingExtractorMixin:
             imaging_extractor.set_times(np.arange(imaging_extractor.get_num_frames() + 1))
 
     @pytest.mark.parametrize("times", [None, np.array([0, 1])])
-    def test_has_time_vector(self, times, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
+    def test_has_time_vector(self, times, imaging_extractor):
         imaging_extractor._times = times
         if times is None:
             assert not imaging_extractor.has_time_vector()
         else:
             assert imaging_extractor.has_time_vector()
 
-    def test_copy_times(self, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor2 = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor._times = np.arange(imaging_extractor.get_num_frames())
+    def test_copy_times(self, imaging_extractor, imaging_extractor2):
+        expected_times = np.arange(imaging_extractor.get_num_frames())
+        imaging_extractor._times = expected_times
         imaging_extractor2.copy_times(imaging_extractor)
-        assert np.array_equal(imaging_extractor._times, imaging_extractor2._times)
-        assert imaging_extractor._times is not imaging_extractor2._times
+        assert np.array_equal(imaging_extractor2._times, expected_times)
+        assert imaging_extractor2._times is not expected_times
 
-    def test_eq(self, imaging_extractor, imaging_extractor_kwargs):
-        imaging_extractor2 = self.imaging_extractor_cls(**imaging_extractor_kwargs)
+    def test_eq(self, imaging_extractor, imaging_extractor2):
         assert imaging_extractor == imaging_extractor2
+
+
+class FrameSliceImagingExtractorMixin:
+    @pytest.fixture(scope="function")
+    def frame_slice_imaging_extractor(self, imaging_extractor):
+        return imaging_extractor.frame_slice(start_frame=1, end_frame=3)
+
+    @pytest.fixture(scope="function")
+    def frame_slice_imaging_extractor2(self, imaging_extractor2):
+        return imaging_extractor2.frame_slice(start_frame=1, end_frame=3)
+
+    @pytest.fixture(scope="function")
+    def frame_slice_expected_video(self, expected_video):
+        return expected_video[1:3]
+
+    def test_get_image_size_frame_slice(self, frame_slice_imaging_extractor, frame_slice_expected_video):
+        image_size = frame_slice_imaging_extractor.get_image_size()
+        assert image_size == (frame_slice_expected_video.shape[1], frame_slice_expected_video.shape[2])
+
+    def test_get_num_frames_frame_slice(self, frame_slice_imaging_extractor, frame_slice_expected_video):
+        num_frames = frame_slice_imaging_extractor.get_num_frames()
+        assert num_frames == frame_slice_expected_video.shape[0]
+
+    def test_get_sampling_frequency_frame_slice(self, frame_slice_imaging_extractor, expected_sampling_frequency):
+        sampling_frequency = frame_slice_imaging_extractor.get_sampling_frequency()
+        assert sampling_frequency == expected_sampling_frequency
+
+    def test_get_dtype_frame_slice(self, frame_slice_imaging_extractor, frame_slice_expected_video):
+        dtype = frame_slice_imaging_extractor.get_dtype()
+        assert dtype == frame_slice_expected_video.dtype
+
+    def test_get_video_frame_slice(self, frame_slice_imaging_extractor, frame_slice_expected_video):
+        video = frame_slice_imaging_extractor.get_video()
+        assert np.array_equal(video, frame_slice_expected_video)
+
+    def test_get_video_slice_frame_slice(self, frame_slice_imaging_extractor, frame_slice_expected_video):
+        video = frame_slice_imaging_extractor.get_video(start_frame=0, end_frame=1)
+        assert np.array_equal(video, frame_slice_expected_video[:1])
+
+    def test_get_video_invalid_start_frame_frame_slice(self, frame_slice_imaging_extractor):
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_video(start_frame=-1)
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_video(start_frame=frame_slice_imaging_extractor.get_num_frames() + 1)
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_video(start_frame=0.5)
+
+    def test_get_video_invalid_end_frame_frame_slice(self, frame_slice_imaging_extractor):
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_video(end_frame=-1)
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_video(end_frame=frame_slice_imaging_extractor.get_num_frames() + 1)
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_video(end_frame=0.5)
+
+    @pytest.mark.parametrize("frame_idxs", [[0], [1], [0, 1], [1, 0]])
+    def test_get_frames_frame_slice(self, frame_slice_imaging_extractor, frame_slice_expected_video, frame_idxs):
+        frames = frame_slice_imaging_extractor.get_frames(frame_idxs=frame_idxs)
+        assert np.array_equal(frames, frame_slice_expected_video[frame_idxs])
+
+    def test_get_frames_invalid_frame_idxs_frame_slice(self, frame_slice_imaging_extractor):
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_frames(frame_idxs=[-1])
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_frames(frame_idxs=[frame_slice_imaging_extractor.get_num_frames()])
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.get_frames(frame_idxs=[0.5])
+
+    @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
+    def test_frame_to_time_no_times_frame_slice(self, frame_slice_imaging_extractor, sampling_frequency):
+        frame_slice_imaging_extractor._times = None
+        frame_slice_imaging_extractor._parent_imaging._sampling_frequency = sampling_frequency
+        times = frame_slice_imaging_extractor.frame_to_time(frames=[0, 1])
+        expected_times = np.array([0, 1]) / sampling_frequency
+        assert np.array_equal(times, expected_times)
+
+    def test_frame_to_time_with_times_frame_slice(self, frame_slice_imaging_extractor):
+        expected_times = np.array([0, 1])
+        frame_slice_imaging_extractor._times = expected_times
+        times = frame_slice_imaging_extractor.frame_to_time(frames=[0, 1])
+
+        assert np.array_equal(times, expected_times)
+
+    @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
+    def test_time_to_frame_no_times_frame_slice(self, frame_slice_imaging_extractor, sampling_frequency):
+        frame_slice_imaging_extractor._times = None
+        frame_slice_imaging_extractor._parent_imaging._sampling_frequency = sampling_frequency
+        times = np.array([0, 1]) / sampling_frequency
+        frames = frame_slice_imaging_extractor.time_to_frame(times=times)
+        expected_frames = np.array([0, 1])
+        assert np.array_equal(frames, expected_frames)
+
+    def test_time_to_frame_with_times_frame_slice(self, frame_slice_imaging_extractor):
+        frame_slice_imaging_extractor._times = np.array([0, 1])
+        times = np.array([0, 1])
+        frames = frame_slice_imaging_extractor.time_to_frame(times=times)
+        expected_frames = np.array([0, 1])
+        assert np.array_equal(frames, expected_frames)
+
+    def test_set_times_frame_slice(self, frame_slice_imaging_extractor):
+        times = np.arange(frame_slice_imaging_extractor.get_num_frames())
+        frame_slice_imaging_extractor.set_times(times)
+        assert np.array_equal(frame_slice_imaging_extractor._times, times)
+
+    def test_set_times_invalid_length_frame_slice(self, frame_slice_imaging_extractor):
+        with pytest.raises(AssertionError):
+            frame_slice_imaging_extractor.set_times(np.arange(frame_slice_imaging_extractor.get_num_frames() + 1))
+
+    @pytest.mark.parametrize("times", [None, np.array([0, 1])])
+    def test_has_time_vector_frame_slice(self, times, frame_slice_imaging_extractor):
+        frame_slice_imaging_extractor._times = times
+        if times is None:
+            assert not frame_slice_imaging_extractor.has_time_vector()
+        else:
+            assert frame_slice_imaging_extractor.has_time_vector()
+
+    def test_copy_times_frame_slice(self, frame_slice_imaging_extractor, frame_slice_imaging_extractor2):
+        expected_times = np.arange(frame_slice_imaging_extractor.get_num_frames())
+        frame_slice_imaging_extractor._times = expected_times
+        frame_slice_imaging_extractor2.copy_times(frame_slice_imaging_extractor)
+        assert np.array_equal(frame_slice_imaging_extractor2._times, expected_times)
+        assert frame_slice_imaging_extractor2._times is not expected_times
+
+    def test_eq_frame_slice(self, frame_slice_imaging_extractor, frame_slice_imaging_extractor2):
+        assert frame_slice_imaging_extractor == frame_slice_imaging_extractor2
--- tests/test_imaging/test_numpy_imaging_extractor.py
+++ tests/test_imaging/test_numpy_imaging_extractor.py
@@ -1,125 +1,45 @@
-from ..mixins.imagingextractor_mixin import ImagingExtractorMixin
+from ..mixins.imagingextractor_mixin import ImagingExtractorMixin, FrameSliceImagingExtractorMixin
 from roiextractors import NumpyImagingExtractor
-from roiextractors.imagingextractor import FrameSliceImagingExtractor
 from roiextractors.testing import generate_dummy_video
 import pytest
 import numpy as np
 
 
-class TestNumpyImagingExtractor(ImagingExtractorMixin):
-    imaging_extractor_cls = NumpyImagingExtractor
+class TestNumpyImagingExtractor(ImagingExtractorMixin, FrameSliceImagingExtractorMixin):
+    @pytest.fixture(scope="class")
+    def expected_video(self):
+        return generate_dummy_video(size=(3, 2, 4))
 
     @pytest.fixture(scope="class")
-    def imaging_extractor_kwargs(self):
-        return dict(timeseries=generate_dummy_video(size=(3, 2, 4)), sampling_frequency=20.0)
+    def expected_sampling_frequency(self):
+        return 20.0
+
+    @pytest.fixture(scope="function")
+    def imaging_extractor(self, expected_video, expected_sampling_frequency):
+        return NumpyImagingExtractor(timeseries=expected_video, sampling_frequency=expected_sampling_frequency)
+
+    @pytest.fixture(scope="function")
+    def imaging_extractor2(self, expected_video, expected_sampling_frequency):
+        return NumpyImagingExtractor(timeseries=expected_video, sampling_frequency=expected_sampling_frequency)
+
+
+class TestNumpyImagingExtractorFromFile(ImagingExtractorMixin, FrameSliceImagingExtractorMixin):
+    @pytest.fixture(scope="class")
+    def expected_video(self):
+        return generate_dummy_video(size=(3, 2, 4))
 
     @pytest.fixture(scope="class")
-    def expected_video(self, imaging_extractor_kwargs):
-        return imaging_extractor_kwargs["timeseries"]
+    def expected_sampling_frequency(self):
+        return 20.0
 
-    @pytest.fixture(scope="class")
-    def expected_sampling_frequency(self, imaging_extractor_kwargs):
-        return imaging_extractor_kwargs["sampling_frequency"]
+    @pytest.fixture(scope="function")
+    def imaging_extractor(self, expected_video, expected_sampling_frequency, tmp_path):
+        file_path = tmp_path / "timeseries.npy"
+        np.save(file_path, expected_video)
+        return NumpyImagingExtractor(timeseries=str(file_path), sampling_frequency=expected_sampling_frequency)
 
-
-class TestNumpyImagingExtractorFromFile(ImagingExtractorMixin):
-    imaging_extractor_cls = NumpyImagingExtractor
-
-    @pytest.fixture(scope="class")
-    def imaging_extractor_kwargs(self, tmp_path_factory):
-        temp_dir = tmp_path_factory.mktemp("data")
-        file_path = temp_dir / "timeseries.npy"
-        timeseries = generate_dummy_video(size=(3, 2, 4))
-        np.save(file_path, timeseries)
-        return dict(timeseries=file_path, sampling_frequency=20.0)
-
-    @pytest.fixture(scope="class")
-    def expected_video(self, imaging_extractor_kwargs):
-        return np.load(imaging_extractor_kwargs["timeseries"])
-
-    @pytest.fixture(scope="class")
-    def expected_sampling_frequency(self, imaging_extractor_kwargs):
-        return imaging_extractor_kwargs["sampling_frequency"]
-
-
-class TestNumpyImagingExtractorFrameSlice(ImagingExtractorMixin):
-    imaging_extractor_cls = FrameSliceImagingExtractor
-
-    @pytest.fixture(scope="class")
-    def imaging_extractor_kwargs(self):
-        parent_kwargs = dict(timeseries=generate_dummy_video(size=(10, 2, 4)), sampling_frequency=20.0)
-        parent_imaging = NumpyImagingExtractor(**parent_kwargs)
-        return dict(parent_imaging=parent_imaging, start_frame=1, end_frame=4)
-
-    @pytest.fixture(scope="class")
-    def expected_video(self, imaging_extractor_kwargs):
-        return imaging_extractor_kwargs["parent_imaging"].get_video(start_frame=1, end_frame=4)
-
-    @pytest.fixture(scope="class")
-    def expected_sampling_frequency(self, imaging_extractor_kwargs):
-        return imaging_extractor_kwargs["parent_imaging"].get_sampling_frequency()
-
-    @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
-    def test_frame_to_time_no_times(self, imaging_extractor_kwargs, sampling_frequency):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor._parent_imaging._times = None
-        imaging_extractor._parent_imaging._sampling_frequency = sampling_frequency
-        times = imaging_extractor.frame_to_time(frames=[0, 1])
-        expected_times = np.array([0, 1]) / sampling_frequency
-        assert np.array_equal(times, expected_times)
-
-    def test_frame_to_time_with_times(self, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor._parent_imaging._times = np.array([0, 1])
-        imaging_extractor._times = np.array([0, 1])
-        times = imaging_extractor.frame_to_time(frames=[0, 1])
-        assert np.array_equal(times, imaging_extractor._parent_imaging._times)
-
-    @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
-    def test_time_to_frame_no_times(self, imaging_extractor_kwargs, sampling_frequency):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor._parent_imaging._times = None
-        imaging_extractor._parent_imaging._sampling_frequency = sampling_frequency
-        imaging_extractor._times = None
-        imaging_extractor._sampling_frequency = sampling_frequency
-        times = np.array([0, 1]) / sampling_frequency
-        frames = imaging_extractor.time_to_frame(times=times)
-        expected_frames = np.array([0, 1])
-        assert np.array_equal(frames, expected_frames)
-
-    def test_time_to_frame_with_times(self, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor._parent_imaging._times = np.array([0, 1])
-        imaging_extractor._times = np.array([0, 1])
-        times = np.array([0, 1])
-        frames = imaging_extractor.time_to_frame(times=times)
-        expected_frames = np.array([0, 1])
-        assert np.array_equal(frames, expected_frames)
-
-    def test_set_times(self, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        times = np.arange(imaging_extractor.get_num_frames())
-        imaging_extractor.set_times(times)
-        assert np.array_equal(imaging_extractor._times, times)
-
-    def test_set_times_invalid_length(self, imaging_extractor):
-        with pytest.raises(AssertionError):
-            imaging_extractor.set_times(np.arange(imaging_extractor.get_num_frames() + 1))
-
-    @pytest.mark.parametrize("times", [None, np.array([0, 1])])
-    def test_has_time_vector(self, times, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor._parent_imaging._times = times
-        imaging_extractor._times = times
-        if times is None:
-            assert not imaging_extractor.has_time_vector()
-        else:
-            assert imaging_extractor.has_time_vector()
-
-    def test_copy_times(self, imaging_extractor_kwargs):
-        imaging_extractor = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor2 = self.imaging_extractor_cls(**imaging_extractor_kwargs)
-        imaging_extractor._times = np.arange(imaging_extractor.get_num_frames())
-        imaging_extractor2.copy_times(imaging_extractor)
-        assert np.array_equal(imaging_extractor._times, imaging_extractor2._times)
-        assert imaging_extractor._times is not imaging_extractor2._times
+    @pytest.fixture(scope="function")
+    def imaging_extractor2(self, expected_video, expected_sampling_frequency, tmp_path):
+        file_path = tmp_path / "timeseries2.npy"
+        np.save(file_path, expected_video)
+        return NumpyImagingExtractor(timeseries=str(file_path), sampling_frequency=expected_sampling_frequency)
