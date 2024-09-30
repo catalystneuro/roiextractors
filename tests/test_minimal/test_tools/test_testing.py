1237fe5f786d48407f8a4593f50b2091e772b9d7
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-09-30 15:44:04 -0700

    added tests for generate_mock_imaging_extractor

--- tests/test_minimal/test_tools/test_testing.py
+++ tests/test_minimal/test_tools/test_testing.py
@@ -1,4 +1,4 @@
-from roiextractors.tools.testing import generate_mock_video
+from roiextractors.tools.testing import generate_mock_video, generate_mock_imaging_extractor
 import pytest
 import numpy as np
 from numpy.testing import assert_array_equal
@@ -23,3 +23,32 @@ def test_generate_mock_video_seed():
     video3 = generate_mock_video(size=size, seed=1)
     assert_array_equal(video1, video2)
     assert not np.array_equal(video1, video3)
+
+
+@pytest.mark.parametrize("num_frames, num_rows, num_columns", [(1, 2, 3), (3, 2, 4), (5, 3, 2)])
+def test_generate_mock_imaging_extractor_shape(num_frames, num_rows, num_columns):
+    imaging_extractor = generate_mock_imaging_extractor(
+        num_frames=num_frames, num_rows=num_rows, num_columns=num_columns
+    )
+    video = imaging_extractor.get_video()
+    assert video.shape == (num_frames, num_rows, num_columns)
+
+
+@pytest.mark.parametrize("sampling_frequency", [10.0, 20.0, 30.0])
+def test_generate_mock_imaging_extractor_sampling_frequency(sampling_frequency):
+    imaging_extractor = generate_mock_imaging_extractor(sampling_frequency=sampling_frequency)
+    assert imaging_extractor.get_sampling_frequency() == sampling_frequency
+
+
+@pytest.mark.parametrize("dtype", [np.uint8, np.float32, "uint8", "float32"])
+def test_generate_mock_imaging_extractor_dtype(dtype):
+    imaging_extractor = generate_mock_imaging_extractor(dtype=dtype)
+    assert imaging_extractor.get_dtype() == np.dtype(dtype)
+
+
+def test_generate_mock_imaging_extractor_seed():
+    imaging_extractor1 = generate_mock_imaging_extractor(seed=0)
+    imaging_extractor2 = generate_mock_imaging_extractor(seed=0)
+    imaging_extractor3 = generate_mock_imaging_extractor(seed=1)
+    assert_array_equal(imaging_extractor1.get_video(), imaging_extractor2.get_video())
+    assert not np.array_equal(imaging_extractor1.get_video(), imaging_extractor3.get_video())
