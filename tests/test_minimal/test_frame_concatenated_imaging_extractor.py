288ad12c22b850554103a801abe8a934e9099795
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-10-07 16:11:24 -0700

    refactored into a mixin

--- tests/test_minimal/test_frame_concatenated_imaging_extractor.py
+++ tests/test_minimal/test_frame_concatenated_imaging_extractor.py
@@ -1,11 +1,16 @@
-from ..mixins.imaging_extractor_mixin import ImagingExtractorMixin, FrameSliceImagingExtractorMixin
+from ..mixins.frame_concatenated_imaging_extractor_mixin import (
+    FrameConcatenatedImagingExtractorMixin,
+    FrameConcatenatedFrameSliceImagingExtractorMixin,
+)
 from roiextractors import NumpyImagingExtractor, FrameConcatenatedImagingExtractor
 from roiextractors.tools.testing import generate_mock_video
 import pytest
-import numpy as np
 
 
-class TestFrameConcatenatedImagingExtractor(ImagingExtractorMixin, FrameSliceImagingExtractorMixin):
+class TestFrameConcatenatedImagingExtractor(
+    FrameConcatenatedImagingExtractorMixin,
+    FrameConcatenatedFrameSliceImagingExtractorMixin,
+):
     @pytest.fixture(scope="class")
     def expected_video(self):
         return generate_mock_video(size=(9, 2, 4))
@@ -35,37 +40,3 @@ class TestFrameConcatenatedImagingExtractor(ImagingExtractorMixin, FrameSliceIma
             )
             imaging_extractors.append(imaging_extractor)
         return FrameConcatenatedImagingExtractor(imaging_extractors=imaging_extractors)
-
-    @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
-    def test_frame_to_time_no_times(self, extractor, sampling_frequency):
-        extractor._times = None
-        extractor._imaging_extractors[0]._sampling_frequency = sampling_frequency
-        times = extractor.frame_to_time(frames=[0, 1])
-        expected_times = np.array([0, 1]) / sampling_frequency
-        assert np.array_equal(times, expected_times)
-
-    @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
-    def test_time_to_frame_no_times(self, extractor, sampling_frequency):
-        extractor._times = None
-        extractor._imaging_extractors[0]._sampling_frequency = sampling_frequency
-        times = np.array([0, 1]) / sampling_frequency
-        frames = extractor.time_to_frame(times=times)
-        expected_frames = np.array([0, 1])
-        assert np.array_equal(frames, expected_frames)
-
-    @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
-    def test_frame_to_time_no_times_frame_slice(self, frame_slice_imaging_extractor, sampling_frequency):
-        frame_slice_imaging_extractor._times = None
-        frame_slice_imaging_extractor._parent_imaging._imaging_extractors[0]._sampling_frequency = sampling_frequency
-        times = frame_slice_imaging_extractor.frame_to_time(frames=[0, 1])
-        expected_times = np.array([0, 1]) / sampling_frequency
-        assert np.array_equal(times, expected_times)
-
-    @pytest.mark.parametrize("sampling_frequency", [1, 2, 3])
-    def test_time_to_frame_no_times_frame_slice(self, frame_slice_imaging_extractor, sampling_frequency):
-        frame_slice_imaging_extractor._times = None
-        frame_slice_imaging_extractor._parent_imaging._imaging_extractors[0]._sampling_frequency = sampling_frequency
-        times = np.array([0, 1]) / sampling_frequency
-        frames = frame_slice_imaging_extractor.time_to_frame(times=times)
-        expected_frames = np.array([0, 1])
-        assert np.array_equal(frames, expected_frames)
