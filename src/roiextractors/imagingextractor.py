b618d291fd578486c78d4d7c068fb5833a7b7388
Heberto Mayorquin <h.mayorquin@gmail.com>
2025-04-25 11:12:04 -0600

    separate string methods to core utils

--- src/roiextractors/imagingextractor.py
+++ src/roiextractors/imagingextractor.py
@@ -11,12 +11,13 @@ FrameSliceImagingExtractor
 from abc import ABC, abstractmethod
 from typing import Union, Optional, Tuple
 from copy import deepcopy
+from math import prod
 import warnings
 
 import numpy as np
 
-from .extraction_tools import ArrayType, PathType, DtypeType, FloatType
-from math import prod
+from .extraction_tools import ArrayType, DtypeType, FloatType
+from .core_utils import _convert_bytes_to_str, _convert_seconds_to_str
 
 
 class ImagingExtractor(ABC):
@@ -30,7 +31,7 @@ class ImagingExtractor(ABC):
         self.name = self.__class__.__name__
         self.is_volumetric = False
 
-    def _repr_text(self):
+    def _repr_text(self) -> str:
         """Generate text representation of the ImagingExtractor object."""
         num_samples = self.get_num_samples()
         sample_shape = self.get_sample_shape()
@@ -45,60 +46,28 @@ class ImagingExtractor(ABC):
 
         # Calculate duration
         duration = num_samples / sf_hz
-        duration_str = self._convert_seconds_to_str(duration)
+        duration_repr = _convert_seconds_to_str(duration)
 
         # Calculate memory size using product of all dimensions in image_size
         memory_size = num_samples * prod(sample_shape) * dtype.itemsize
-        memory_str = self._convert_bytes_to_str(memory_size)
+        memory_repr = _convert_bytes_to_str(memory_size)
 
-        # Format shape string based on whether it's volumetric or not
-
-        sample_shape_string = f"{sample_shape[0]} rows x {sample_shape[1]} columns "
+        # Format shape string based on whether data is volumetric or not
+        sample_shape_repr = f"{sample_shape[0]} rows x {sample_shape[1]} columns "
         if self.is_volumetric:
-            sample_shape_string += f"x {sample_shape[2]} planes"
+            sample_shape_repr += f"x {sample_shape[2]} planes"
 
         return (
             f"{self.name}\n"
-            f"  Number of samples: {num_samples} \n"
-            f"  Sample shape: {sample_shape_string} \n"
+            f"  Number of samples: {num_samples:,} \n"
+            f"  Sample shape: {sample_shape_repr} \n"
             f"  Sampling rate: {sampling_frequency_repr}\n"
-            f"  Duration: {duration_str}\n"
-            f"  Imaging data memory: {memory_str} ({dtype} dtype)"
+            f"  Duration: {duration_repr}\n"
+            f"  Imaging data memory: {memory_repr} ({dtype} dtype)"
         )
 
     def __repr__(self):
         return self._repr_text()
-
-    def _convert_seconds_to_str(self, seconds):
-        """Convert seconds to a human-readable string."""
-        if seconds < 60:
-            return f"{seconds:.1f}s"
-        elif seconds < 3600:
-            minutes = seconds / 60
-            return f"{minutes:.1f}min"
-        else:
-            hours = seconds / 3600
-            return f"{hours:.1f}h"
-
-    def _convert_bytes_to_str(self, size_in_bytes):
-        """
-        Convert bytes to a human-readable string.
-
-        Convert bytes to a human-readable string using IEC binary prefixes (KiB, MiB, GiB).
-        Note that RAM memory is typically measured in IEC binary prefixes  while disk storage is typically
-        measured in SI binary prefixes.
-        """
-        if size_in_bytes < 1024:
-            return f"{size_in_bytes}B"
-        elif size_in_bytes < 1024 * 1024:
-            size_kb = size_in_bytes / 1024
-            return f"{size_kb:.1f}KiB"
-        elif size_in_bytes < 1024 * 1024 * 1024:
-            size_mb = size_in_bytes / (1024 * 1024)
-            return f"{size_mb:.1f}MiB"
-        else:
-            size_gb = size_in_bytes / (1024 * 1024 * 1024)
-            return f"{size_gb:.1f}GiB"
 
     @abstractmethod
     def get_image_shape(self) -> Tuple[int, int]:
